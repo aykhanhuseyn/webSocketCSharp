668b836a245f49e992142139a6165a719cdce0a1
Aykhan Huseynli <immense_ideas@icloud.com>
2020-02-24 12:05:29 +0400

    initial

---
+++ dotNetFourWebSocket/Program.cs
@@ -0,0 +1,48 @@
+﻿using SuperWebSocket;
+using System;
+
+namespace dotNetFourWebSocket
+{
+    class Program
+    {
+        private static WebSocketServer wsServer;
+        static void Main(string[] args)
+        {
+            wsServer = new WebSocketServer();
+            int port = 8080;
+            wsServer.Setup(port);
+            wsServer.NewSessionConnected += WsServer_NewSessionConnected;
+            wsServer.NewMessageReceived += WsServer_NewMessageReceived;
+            wsServer.NewDataReceived += WsServer_NewDataReceived;
+            wsServer.SessionClosed += WsServer_SessionClosed;
+            wsServer.Start();
+            Console.WriteLine("Server is running on port: " + port + "; Press and key to exit...");
+            Console.ReadLine();
+            wsServer.Stop();
+        }
+
+        private static void WsServer_SessionClosed(WebSocketSession session, SuperSocket.SocketBase.CloseReason value)
+        {
+            Console.WriteLine("session closed");
+        }
+
+        private static void WsServer_NewDataReceived(WebSocketSession session, byte[] value)
+        {
+            Console.WriteLine("data received");
+        }
+
+        private static void WsServer_NewMessageReceived(WebSocketSession session, string value)
+        {
+            Console.WriteLine("message received: " + value);
+            if(value == "Hello Server")
+            {
+                session.Send("Hello client");
+            }
+        }
+
+        private static void WsServer_NewSessionConnected(WebSocketSession session)
+        {
+            Console.WriteLine("session connected");
+        }
+    }
+}
